524969342cdbeae937ae1c19722f87708da4bf4e
444MINEL <116800147+Kujundzic-Minel@users.noreply.github.com>
2023-05-15 21:00:54 +0200

    reset à 0

--- Kyks/src/main.js
+++
@@ -1,8 +0,0 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
-import App from './App.vue'
-import Vue3Geolocation from 'vue3-geolocation';
-
-createApp(App).mount('#app')
-app.use(VueGeolocation);
