362d295f9781bca34699f8fd321ad463173e511f
John Doe <johndoe@example.com>
2023-05-15 19:12:53 +0200

    début installation api google map

---
+++ Kyks/src/components/GoogleMaps.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <p>Google maps component</p>
+    </div>
+</template>
+
+<script>
+
+    export default() {
+        created() {
+            this.$getLocation()
+            .then((coordinates) => {
+        console.log(coordinates);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+};
+</script>
--- Kyks/src/main.js
+++ Kyks/src/main.js
@@ -2,5 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import Vue3Geolocation from 'vue3-geolocation';
 
 createApp(App).mount('#app')
+app.use(VueGeolocation);
