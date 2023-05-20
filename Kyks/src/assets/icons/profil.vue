10bd3b80d4057f53dd0b6724dd48fe00dcff4e1f
444MINEL <116800147+Kujundzic-Minel@users.noreply.github.com>
2023-05-20 10:21:38 +0200

    ajout icon/composant navigation page

---
+++ Kyks/src/assets/icons/profil.vue
@@ -0,0 +1,5 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="none" viewBox="0 0 32 32">
+    <path fill="#fff" d="M16 14.667A5.333 5.333 0 1 0 16 4a5.333 5.333 0 0 0 0 10.666ZM24 28a1.333 1.333 0 0 0 1.333-1.333 9.333 9.333 0 1 0-18.666 0A1.333 1.333 0 0 0 8 28h16Z"/>
+  </svg>
+</template>
