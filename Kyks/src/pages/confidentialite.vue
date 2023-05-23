3db7673e9ab21fc89a4bab2b62144c93c36eb217
John Doe <johndoe@example.com>
2023-05-23 15:49:55 +0200

    page confidentalité faite

--- Kyks/src/pages/confidentialite.vue
+++ Kyks/src/pages/confidentialite.vue
@@ -5,17 +5,24 @@
   <main>
     <div class="mt-20">
       <h1 class="text-xl font-bold text-purple-500 text-center mb-3">Confidentialité</h1>
-      <p class="mx-7 mb-7" >Politique de confidentialité
-      Nous attachons une grande importance à la confidentialité de vos informations personnelles. Cette politique de confidentialité explique comment nous collectons, utilisons, divulguons et protégeons vos données lors de votre utilisation de notre site web. Veuillez prendre le temps de lire attentivement cette politique pour comprendre nos pratiques en matière de confidentialité.
       
-      Collecte des informations
-      Nous pouvons collecter des informations personnelles vous concernant lorsque vous visitez notre site web, vous vous inscrivez à nos services, effectuez des achats ou interagissez avec notre plateforme de quelque manière que ce soit. Les informations personnelles que nous pouvons collecter comprennent, sans s'y limiter, votre nom, votre adresse e-mail, votre numéro de téléphone, votre adresse postale et des données de transaction.
+      <h1 class="mx-7 mb-7 font-bold text-gray-500">Politique de confidentialité</h1>
+      <p class="mx-7 mb-7" >Nous attachons une grande importance à la confidentialité de vos informations personnelles. Cette politique de confidentialité explique comment nous collectons, utilisons, divulguons et protégeons vos données lors de votre utilisation de notre site web. Veuillez prendre le temps de lire attentivement cette politique pour comprendre nos pratiques en matière de confidentialité.</p>
       
-      Utilisation des informations
-      Nous utilisons les informations personnelles que nous collectons dans le but de vous fournir nos services, d'améliorer votre expérience utilisateur et de répondre à vos demandes. Ces informations peuvent également être utilisées pour communiquer avec vous, vous informer sur nos produits, services et offres spéciales, et effectuer des analyses statistiques.
+     
+      <h1 class="mx-7 mb-7 font-bold text-gray-500">Collecte des informations</h1>
+      <p class="mx-7 mb-7">Nous pouvons collecter des informations personnelles vous concernant lorsque vous visitez notre site web, vous vous inscrivez à nos services, effectuez des achats ou interagissez avec notre plateforme de quelque manière que ce soit. Les informations personnelles que nous pouvons collecter comprennent, sans s'y limiter, votre nom, votre adresse e-mail, votre numéro de téléphone, votre adresse postale et des données de transaction.
+      </p>
       
-      Sécurité des informations
-      Nous mettons en œuvre des mesures de sécurité appropriées pour protéger vos informations personnelles contre tout accès non autorisé, toute divulgation, toute altération ou toute destruction. Cependant, veuillez noter qu'aucune méthode de transmission sur Internet ou de stockage électronique n'est totalement sécurisée, et nous ne pouvons garantir une sécurité absolue de vos informations.</p>
+      
+        <h1 class="mx-7 mb-7 font-bold text-gray-500">Utilisation des informations</h1>
+      <p class="mx-7 mb-7">Nous utilisons les informations personnelles que nous collectons dans le but de vous fournir nos services, d'améliorer votre expérience utilisateur et de répondre à vos demandes. Ces informations peuvent également être utilisées pour communiquer avec vous, vous informer sur nos produits, services et offres spéciales, et effectuer des analyses statistiques.
+      </p>
+      
+      
+      <h1 class="mx-7 mb-7 font-bold text-gray-500">Sécurité des informations</h1>
+      <p class="mx-7 mb-7">Nous mettons en œuvre des mesures de sécurité appropriées pour protéger vos informations personnelles contre tout accès non autorisé, toute divulgation, toute altération ou toute destruction. Cependant, veuillez noter qu'aucune méthode de transmission sur Internet ou de stockage électronique n'est totalement sécurisée, et nous ne pouvons garantir une sécurité absolue de vos informations.
+      </p>
     </div>
   </main>
 </template>
