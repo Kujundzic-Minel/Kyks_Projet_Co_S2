f9657c932c49bb1e3cfeb2b99d7d4d3e4ce45585
444MINEL <116800147+Kujundzic-Minel@users.noreply.github.com>
2023-06-04 17:44:39 +0200

    page securité faite

--- Kyks/src/pages/securite.vue
+++ Kyks/src/pages/securite.vue
@@ -3,6 +3,34 @@
 
 <template>
   <main>
-    <h1>Page page securite</h1>
+    <h1 class="font-bold text-xl text-purple-500 px-8 text-center">Page sécurité</h1>
+    <p class="px-6 mb-5 mt-3">
+      La sécurité des utilisateurs est une priorité absolue sur notre site web de recherche d'équipiers pour jouer au football. Nous avons mis en place des mesures de sécurité pour garantir que votre expérience sur notre plateforme soit sécurisée et agréable. Voici quelques informations importantes à connaître :
+
+      
+        <h1 class="font-bold text-xl text-purple-500 px-8">Protection des données personnelles :</h1>
+        <p class="px-6 mb-5 mt-3">Nous comprenons l'importance de la confidentialité de vos données personnelles. Toutes les informations que vous partagez avec nous sont traitées de manière confidentielle et conformément à notre politique de confidentialité.
+        Nous ne partagerons jamais vos données personnelles avec des tiers sans votre consentement explicite. Vos informations ne seront utilisées que dans le but de faciliter votre recherche d'équipiers et d'améliorer notre service.
+      </p>
+
+      
+        <h1 class="font-bold text-xl text-purple-500 px-8">Sécurité du compte utilisateur :</h1>
+        <p class="px-6 mb-5 mt-3">Lors de votre inscription, veillez à choisir un mot de passe fort et unique. Nous vous recommandons d'utiliser une combinaison de lettres, de chiffres et de caractères spéciaux pour renforcer la sécurité de votre compte.
+        Nous vous encourageons également à activer l'authentification à deux facteurs (2FA) pour une sécurité accrue. Le 2FA ajoute une couche de protection supplémentaire en demandant un code de vérification lors de la connexion à votre compte.
+      </p>
+
+      
+        <h1 class="font-bold text-xl text-purple-500 px-8">Signalement des comportements abusifs :</h1>
+        <p class="px-6 mb-5 mt-3">Nous nous efforçons de maintenir un environnement sûr et respectueux pour tous les utilisateurs. Si vous rencontrez un comportement abusif, harcelant ou inapproprié de la part d'un autre utilisateur, veuillez nous en informer immédiatement en utilisant nos canaux de signalement.
+        Nous examinerons toutes les plaintes et prendrons les mesures appropriées pour assurer la sécurité de notre communauté.
+      </p>
+
+      
+        <h1 class="font-bold text-xl text-purple-500 px-8">Précautions lors des rencontres en personne :</h1>
+        <p class="px-6 mb-5 mt-3">Lorsque vous décidez de rencontrer des équipiers pour jouer au football en personne, veillez à prendre des précautions de sécurité supplémentaires. Choisissez des lieux publics bien éclairés et informez quelqu'un de confiance de vos plans.
+        Il est recommandé de jouer avec des équipiers que vous avez pu rencontrer et connaître à travers notre plateforme, plutôt que de rencontrer des inconnus.
+        Nous nous engageons à maintenir un environnement sûr et sécurisé pour tous les utilisateurs de notre site web. Si vous avez des préoccupations ou des questions supplémentaires concernant la sécurité, n'hésitez pas à nous contacter. Votre sécurité est notre priorité absolue.
+      </p>
+</p>
   </main>
 </template>
