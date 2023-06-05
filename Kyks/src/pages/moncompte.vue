255d7a4070637b57e4ed190b0c209ccc05ed8efe
John Doe <johndoe@example.com>
2023-06-05 13:23:03 +0200

    fix formulaire

--- Kyks/src/pages/moncompte.vue
+++ Kyks/src/pages/moncompte.vue
@@ -70,7 +70,7 @@ const doCreateAccount = async () => {
           <button type="button" @click="doLogout"
             class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Logout</button>
 
-        </div>
+        </div>  
       </div>
       <div v-else>
         <h1>{{ loginMode ? "LOGIN" : "CREATE ACCOUNT" }}</h1>
