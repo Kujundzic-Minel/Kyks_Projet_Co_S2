c4029c36c6a41c7a0137a4acf28fa8c853287d0b
John Doe <johndoe@example.com>
2023-06-05 15:20:36 +0200

    modification cardsevent

--- Kyks/src/components/cardsevent.vue
+++ Kyks/src/components/cardsevent.vue
@@ -30,7 +30,9 @@ const rejoindre = () => {
                 <p class="text-center text-purple-800 font-semibold">{{ lieu_event }}</p>
                 <span class="absolute right-0 bg-orange-400 rounded-full pr-2 -mt-2 p-2 -mr-3 max-w-[31px] max-h-[31px] align-middle">{{ nb_participants }}</span>
                 
-                <RouterLink class="inline-block border-b-2 border-black uppercase" :to="`${url}`">Rejoindre</RouterLink>
+                <div class="bg-orange-500 rounded-xl">
+                    <RouterLink class="pl-1 font-bold text-white" :to="`${url}`">Rejoindre</RouterLink>
+                </div>
                 
             </div>
         </section>
